4d8404a1979b048ef325520094c99c08298e5dfb
KamilKrol <kamil.h.krol@gmail.com>
2015-12-13 18:05:24 +0100

    App main.

---
+++ 3-chapter/src/Main3.scala
@@ -0,0 +1,9 @@
+/**
+  * Created by kamil on 13.12.15.
+  */
+object Main3 extends App {
+
+  println("New version of main")
+  println("Command-line arguments: " + args.mkString("[", ",", "]"))
+
+}
