457ee535cbd7dc76a996d0da3774ea55a699a87b
KamilKrol <kamil.h.krol@gmail.com>
2015-12-13 10:56:08 +0100

    Initial commit

---
+++ 3-chapter/src/Main1.scala
@@ -0,0 +1,16 @@
+/**
+  * Created by kamil on 13.12.15.
+  */
+class Main1 {
+
+  def main(args: Array[String]) {
+    var immutableSet = Set(1, 2, 3)
+    immutableSet += 4
+    println(immutableSet.mkString(", "))
+
+    val mutableSet = scala.collection.mutable.Set(1, 2, 3)
+    mutableSet += 4
+    println(mutableSet.mkString(", "))
+  }
+
+}
